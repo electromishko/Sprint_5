38d0eaa3083a7665bf1e41478a04fbca7997e6b2
sekretarev_m <sekretarev_m@protei.ru>
2025-09-21 12:43:41 +0300

    Создание пользователей переделаны в отдельные сценарии

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -3,62 +3,75 @@ from selenium.webdriver.support import expected_conditions
 from faker import Faker
 from locators import AuthorizationLocators
 import data
-import pytest
 
 faker = Faker()
 
 class TestUICreateUser:
 
-    @pytest.mark.parametrize(
-        'login, password, case_number',
-        [
-            [faker.ascii_free_email(), faker.password(), 1],
-            [faker.text(15), faker.password(), 2],
-            ['electro@mail.ru', '666666', 3]
-        ]
-    )
-    def test_user_creation(self, driver, create_user, login, password, case_number):
+    def test_register_valid_user_succesful(self, driver):
         driver.get(data.web_link)
-        WebDriverWait(driver, 10).until(
-            expected_conditions.element_to_be_clickable(AuthorizationLocators.LOGIN_BUTTON)
-        )
         driver.find_element(*AuthorizationLocators.LOGIN_BUTTON).click()
-
         WebDriverWait(driver, 10).until(
             expected_conditions.visibility_of_element_located(AuthorizationLocators.NO_ACCOUNT_BUTTON)
         )
         driver.find_element(*AuthorizationLocators.NO_ACCOUNT_BUTTON).click()
 
-        if case_number == 3:
-            login = create_user[0]
-            password = create_user[1]
+        login = faker.email()
+        password = faker.password()
         driver.find_element(*AuthorizationLocators.EMAIL_FIELD).send_keys(login)
         driver.find_element(*AuthorizationLocators.PASSWORD_FIELD).send_keys(password)
         driver.find_element(*AuthorizationLocators.CONFIRM_PASSWORD_FIELD).send_keys(password)
         driver.find_element(*AuthorizationLocators.CREATE_ACCOUNT_BUTTON).click()
 
-        if case_number == 1:
-            WebDriverWait(driver, 10).until(
-                expected_conditions.visibility_of_element_located(AuthorizationLocators.USER_TEXT)
-            )
-            user_text = driver.find_element(*AuthorizationLocators.USER_TEXT).text
-            assert user_text == 'User.'
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(AuthorizationLocators.USER_TEXT))
+        user_text = driver.find_element(*AuthorizationLocators.USER_TEXT).text
+        assert user_text == 'User.'
 
-        elif case_number == 2:
-            WebDriverWait(driver, 10).until(
-                expected_conditions.visibility_of_element_located(AuthorizationLocators.ERROR_TEXT)
-            )
-            error_text = driver.find_element(*AuthorizationLocators.ERROR_TEXT).text
-            assert error_text == 'Ошибка'
+    def test_register_invalid_email_error(self, driver):
+        driver.get(data.web_link)
+        driver.find_element(*AuthorizationLocators.LOGIN_BUTTON).click()
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(AuthorizationLocators.NO_ACCOUNT_BUTTON))
+        driver.find_element(*AuthorizationLocators.NO_ACCOUNT_BUTTON).click()
 
-            assert driver.find_element(*AuthorizationLocators.EMAIL_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
-            assert driver.find_element(*AuthorizationLocators.PASSWORD_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
-            assert driver.find_element(*AuthorizationLocators.CONFIRM_PASSWORD_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
+        login = faker.text(15)  # Невалидный email
+        password = faker.password()
+        
+        driver.find_element(*AuthorizationLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(*AuthorizationLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(*AuthorizationLocators.CONFIRM_PASSWORD_FIELD).send_keys(password)
+        driver.find_element(*AuthorizationLocators.CREATE_ACCOUNT_BUTTON).click()
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(AuthorizationLocators.ERROR_TEXT))
+        
+        error_text = driver.find_element(*AuthorizationLocators.ERROR_TEXT).text
+        assert error_text == 'Ошибка'
+        
+        assert driver.find_element(*AuthorizationLocators.EMAIL_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
+        assert driver.find_element(*AuthorizationLocators.PASSWORD_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
+        assert driver.find_element(*AuthorizationLocators.CONFIRM_PASSWORD_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
 
-        elif case_number == 3:
-            WebDriverWait(driver, 10).until(
-                expected_conditions.visibility_of_element_located(AuthorizationLocators.ERROR_TEXT)
-            )
-            error_text = driver.find_element(*AuthorizationLocators.ERROR_TEXT).text
-            assert error_text == 'Ошибка'
+    def test_register_existing_user_error(self, driver):
+        driver.get(data.web_link)
+        driver.find_element(*AuthorizationLocators.LOGIN_BUTTON).click()
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(AuthorizationLocators.NO_ACCOUNT_BUTTON)
+        )
+        driver.find_element(*AuthorizationLocators.NO_ACCOUNT_BUTTON).click()
+       
+        driver.find_element(*AuthorizationLocators.EMAIL_FIELD).send_keys(data.EXISTING_USER['email'])
+        driver.find_element(*AuthorizationLocators.PASSWORD_FIELD).send_keys(data.EXISTING_USER['password'])
+        driver.find_element(*AuthorizationLocators.CONFIRM_PASSWORD_FIELD).send_keys(data.EXISTING_USER['password'])
+        driver.find_element(*AuthorizationLocators.CREATE_ACCOUNT_BUTTON).click()
 
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(AuthorizationLocators.ERROR_TEXT)
+        )
+        
+        error_text = driver.find_element(*AuthorizationLocators.ERROR_TEXT).text
+        assert error_text == 'Ошибка'
+        
+        assert driver.find_element(*AuthorizationLocators.EMAIL_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
+        assert driver.find_element(*AuthorizationLocators.PASSWORD_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
+        assert driver.find_element(*AuthorizationLocators.CONFIRM_PASSWORD_CONTAINER).value_of_css_property("border-color") == 'rgb(255, 105, 114)', 'цвет ошибки поля'
